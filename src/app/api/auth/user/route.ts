7a35d7cc0a2613294caa89f5d28d1786e42cdea1
Mr. Hasan <hasanshahriar32@gmail.com>
2023-12-02 10:54:30 +0600

    updated

--- src/app/api/auth/user/route.ts
+++ src/app/api/auth/user/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
 // import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-export async function GET() {
+async function GET() {
   // const session = null;
   // // const session = await getServerSession(authOptions);
   // console.log(session);
